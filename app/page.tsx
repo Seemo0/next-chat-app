2967c906660010249fee47c328968b4f336c7370
Seemo0 <mouallal@valhko.com>
2024-01-18 22:39:07 +0100

    add details

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import AboutChat from "@/components/AboutChat";
 import ChatHeader from "@/components/ChatHeader";
+import ChatInput from "@/components/ChatInput";
+import ChatMessages from "@/components/ChatMessages";
+import InitUser from "@/lib/store/initUser";
 import { supabaseServer } from "@/lib/supabase/server";
 
 export default async function Home() {
@@ -7,10 +11,21 @@ export default async function Home() {
   console.log("data====>", data.session?.user);
 
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
-      <div className=" h-full border rounded-md flex flex-col relative">
-        <ChatHeader user={data?.session?.user} />
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className=" h-full border rounded-md flex flex-col relative">
+          <ChatHeader user={data?.session?.user} />
+          {data.session?.user ? (
+            <>
+              <ChatMessages />
+              <ChatInput />
+            </>
+          ) : (
+            <AboutChat />
+          )}
+        </div>
       </div>
-    </div>
+      <InitUser user={data.session?.user} />
+    </>
   );
 }
