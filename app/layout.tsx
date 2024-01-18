684814a20ca557b1a521f86545d1add603a257c2
Seemo0 <mouallal@valhko.com>
2024-01-19 00:30:39 +0100

    add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={space_Grotesk.className} >
+      <body className={space_Grotesk.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -26,6 +27,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster position="top-center" />
         </ThemeProvider>
       </body>
     </html>
