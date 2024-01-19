7f6c5444ae8de843f8812b6d69fef13a5082345d
Seemo0 <mouallal@valhko.com>
2024-01-19 23:59:39 +0100

    updates

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { supabaseBrowser } from "@/lib/supabase/browser";
-import ChatIcon from "./ChatIcon";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import LiveChat from "./LiveChat";
 
 const ChatHeader = ({ user }: { user?: any }) => {
   const router = useRouter();
@@ -31,7 +31,7 @@ const ChatHeader = ({ user }: { user?: any }) => {
       <div className="p-5 border-b flex items-center justify-between h-full">
         <div>
           <h1 className="text-xl font-bold">Daily Chat</h1>
-          <ChatIcon />
+          <LiveChat />
         </div>
         {user ? (
           <Button onClick={handleLogout}>Logout</Button>
