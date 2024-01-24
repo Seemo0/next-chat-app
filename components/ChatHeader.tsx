ed17b8d73b1ee1ccda85726479c70f20f6855fb6
Seemo0 <mouallal@valhko.com>
2024-01-24 20:00:22 +0100

    test: change the callback to prod version

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -15,7 +15,7 @@ const ChatHeader = ({ user }: { user?: any }) => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: location.origin + "/auth/callback",
+        redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL,
       },
     });
   };
